e9408cbd8010c5d01af0e91cc23af7ed60d60bf2
Tallesmello <tallesmellog12@gmail.com>
2021-09-22 17:12:11 -0300

    Coloquei todas as telas em binding.

--- app/src/main/java/com/example/dont_waste_brq/activity/HomeNaoLogadaActivity.kt
+++ app/src/main/java/com/example/dont_waste_brq/activity/HomeNaoLogadaActivity.kt
@@ -6,30 +6,27 @@ import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.ViewModelProvider
 import com.example.dont_waste_brq.R
+import com.example.dont_waste_brq.databinding.ActivityHomeNaoLogadaBinding
 import com.example.dont_waste_brq.viewmodel.HomeNaoLogadaViewModel
 
 
 class HomeNaoLogadaActivity : AppCompatActivity() {
 
     private lateinit var viewmodel : HomeNaoLogadaViewModel
-    lateinit var btnPrimeiroAcesso: AppCompatButton
-    lateinit var btnLoginHomeNaoLogada: AppCompatButton
-
+    private lateinit var binding: ActivityHomeNaoLogadaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_nao_logada)
+        binding = ActivityHomeNaoLogadaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        btnPrimeiroAcesso = findViewById(R.id.btn_primeiro_acesso_hm_n_logada)
-        btnLoginHomeNaoLogada = findViewById(R.id.btn_login_hm_n_logada)
-
-
-        btnPrimeiroAcesso.setOnClickListener {
+        binding.btnPrimeiroAcessoHmNLogada.setOnClickListener {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
         }
 
-        btnLoginHomeNaoLogada.setOnClickListener {
+        binding.btnLoginHmNLogada.setOnClickListener {
             val intentLogar = Intent(this, LoginActivity::class.java)
             startActivity(intentLogar)
         }
