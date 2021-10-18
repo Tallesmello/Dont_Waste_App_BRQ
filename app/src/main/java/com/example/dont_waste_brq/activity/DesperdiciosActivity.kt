48e81b873925cf04a38fa31236bf926eff76c65e
Beatriz <beatriz.ocgl@gmail.com>
2021-10-18 15:45:27 -0300

    Criação do menu hamburguer e barra de pesquisa

--- app/src/main/java/com/example/dont_waste_brq/activity/DesperdiciosActivity.kt
+++ app/src/main/java/com/example/dont_waste_brq/activity/DesperdiciosActivity.kt
@@ -1,4 +1,5 @@
 package com.example.dont_waste_brq.activity
+
 import com.example.dont_waste_brq.activity.BaseActivity
 
 
@@ -13,12 +14,13 @@ import com.example.dont_waste_brq.R
 import com.example.dont_waste_brq.activity.PrimeiroAcessoActivity
 import com.example.dont_waste_brq.databinding.ActivityPrimeiroAcessoBinding
 import com.example.dont_waste_brq.databinding.ActivityRelatorioDeDesperdicioBinding
+import com.miguelcatalan.materialsearchview.MaterialSearchView
 import com.example.dont_waste_brq.activity.VencimentosProximosActivity as VencimentosProximosActivity
 
 class DesperdiciosActivity : BaseActivity() {
 
-    private lateinit var binding : ActivityRelatorioDeDesperdicioBinding
-    private lateinit var appBar : ToolbarWidgetWrapper
+    private lateinit var binding: ActivityRelatorioDeDesperdicioBinding
+    private lateinit var searchView: MaterialSearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,27 +28,44 @@ class DesperdiciosActivity : BaseActivity() {
         val view = binding.root
         setContentView(view)
 
-      val toolbar = binding.toolbarDesperdicios.toolbar
+        inicializarComponentes()
+
+        val toolbar = binding.toolbarDesperdicios.toolbar
+        toolbar.title = " "
         setSupportActionBar(toolbar)
 
 
-
-
     }
 
+    private fun inicializarComponentes() {
+        searchView = binding.toolbarDesperdicios.searchView
+    }
+
+
+    /**
+     * Criação do menu hamburguer em appBar
+     */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater : MenuInflater = menuInflater
+        val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.icon_appbar, menu)
+
+        /**
+         * Configurar botão pesquisa (SearchView)
+         */
+        val item: MenuItem = menu!!.findItem(R.id.pesquisa)
+        searchView.setMenuItem(item)
         return super.onCreateOptionsMenu(menu)
     }
 
+    /**
+     * Configurar comportamentos do menu em appBar
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         R.id.action_settings.apply {
             trocarTela(ActivityMenu())
         }
         return super.onOptionsItemSelected(item)
     }
-
 
 
 }
