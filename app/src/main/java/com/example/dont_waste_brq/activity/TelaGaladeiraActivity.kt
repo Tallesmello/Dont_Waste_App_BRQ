539835a35ddfd564ce53f0a60c7f2ddac0e20c64
Talles Mello <tallesmello@gmail.com>
2021-10-20 17:57:10 -0300

    telas geladeira pronta

--- app/src/main/java/com/example/dont_waste_brq/activity/TelaGaladeiraActivity.kt
+++ app/src/main/java/com/example/dont_waste_brq/activity/TelaGaladeiraActivity.kt
@@ -2,11 +2,71 @@ package com.example.dont_waste_brq.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.size
 import com.example.dont_waste_brq.R
+import com.example.dont_waste_brq.databinding.ActivityHomeLogadaBinding
+import com.example.dont_waste_brq.databinding.ActivityTelaGaladeiraBinding
+import com.miguelcatalan.materialsearchview.MaterialSearchView
 
-class TelaGaladeiraActivity : AppCompatActivity() {
+class TelaGaladeiraActivity : BaseActivity(){
+
+    private lateinit var binding: ActivityTelaGaladeiraBinding
+    private lateinit var searchView: MaterialSearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_galadeira)
+        binding = ActivityTelaGaladeiraBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        inicializaçãoComponentes()
+        inicializaçãoToolbar()
+
+
+    }
+    private fun inicializaçãoToolbar() {
+        val toolbar = binding.appBarTelaGeladeira.toolbar
+        toolbar.title = " "
+        setSupportActionBar(toolbar)
+    }
+
+    /**
+     * Inicialização da barra de pesquisa
+     * em appBar
+     */
+    private fun inicializaçãoComponentes() {
+        searchView = binding.appBarTelaGeladeira.searchView
+    }
+
+
+    /**
+     * Inflando icones em appBar
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.icon_appbar, menu)
+
+        /**
+         * Configurar botão pesquisa (SearchView)
+         */
+        val item : MenuItem = menu!!.findItem(R.id.pesquisa)
+        searchView.setMenuItem(item)
+        val size = searchView.size
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /**
+     * Colocando comportamentos no icone menu hamburger
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        R.id.menu_hamburguer.apply {
+            trocarTela(MenuHamburguer())
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
