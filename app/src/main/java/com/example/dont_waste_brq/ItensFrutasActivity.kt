ffae5add5967b1dde3e69763dc02e17729455b48
Talles Mello <tallesmello@gmail.com>
2021-11-03 15:16:32 -0300

    itens layout

---
+++ app/src/main/java/com/example/dont_waste_brq/ItensFrutasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dont_waste_brq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItensFrutasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_itens_frutas)
+    }
+}
