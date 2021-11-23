70b5b88f0e39bc6bd89b6979c51f53600768374b
torto <gcarmo444@gmail.com>
2021-11-23 15:59:53 -0300

    falha resolvida

--- app/src/main/java/com/example/dont_waste_brq/activity/AlimentosCadastradosActivity.kt
+++ app/src/main/java/com/example/dont_waste_brq/activity/AlimentosCadastradosActivity.kt
@@ -1,11 +1,8 @@
 package com.example.dont_waste_brq.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dont_waste_brq.R
 import com.example.dont_waste_brq.activity.adapter.ProdutoAdapter
 import com.example.dont_waste_brq.databinding.ActivityAlimentosCadastradosBinding
-import com.example.dont_waste_brq.databinding.ActivityItensFrutasBinding
 
 class AlimentosCadastradosActivity : BaseActivity() {
 
