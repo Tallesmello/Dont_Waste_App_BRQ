22f8d9527dfebdc41eddd00c8c4362648d766976
Beatriz <beatriz.ocgl@gmail.com>
2022-01-12 17:27:28 -0300

    testes unitarios classe Usuario

---
+++ app/src/test/java/com/example/dont_waste_brq/model/UsuarioTest.kt
@@ -0,0 +1,16 @@
+package com.example.dont_waste_brq.model
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class UsuarioTest {
+
+    @Test
+    fun getEmail() {
+    }
+
+    @Test
+    fun getSenha() {
+    }
+}
